33000a09e0170efdc533bf9e5fa871aee689eb86
pravin khandare <pravinkhandare2503@gmail.com>
2025-08-18 12:39:06 +0530

    latest

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "./CSS/LoginSignup.css"
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsinup'>
+      <div className="login-contaner">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fileds">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Your Email'/>
+          <input type="passworld" placeholder='Passworld'/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Allredy have an acount <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continue, i agree to the terms of use privacy and policy.</p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
--- frontend/src/Pages/SoftCategery.jsx
+++ frontend/src/Pages/SoftCategery.jsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import "./CSS/ShopCategory.css"
 import { ShopContex } from '../Contex/ShopContex'
+import dropdowm_icon from '../components/Asset/dropdown_icon.png'
+import Item from '../components/Items/Items'
 
 const SoftCategery = (props) => {
-  const {all_product} = useState(ShopContex)
+  const { all_product } = useContext(ShopContex)  // ✅ useContext instead of useState
+
   return (
     <div className='shop-categerory'>
-      <img src={props.banner} alt="" />
+      <img className='shopcategerory-banner' src={props.banner} alt="" />
       <div className="shopcategerory-indexSort">
-        
+        <p>
+          <span> Showing 1-12</span> out of 36 product
+        </p>
+        <div className="shopcategerory-sort"> 
+          Sort by <img src={dropdowm_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategerory-products">
+        {all_product && all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            )
+          } else {
+            return null
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More 
       </div>
     </div>
   )
